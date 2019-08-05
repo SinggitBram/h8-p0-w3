cae5cbfb5b924c52ba3295b06ffc6bab192384d8
Singgit Bramantha <bambadom@gmail.com>
2019-08-05 15:01:37 +0700

    revisi exercise2

--- exercise2.js
+++ exercise2.js
@@ -1,12 +1,14 @@
 
-var pengulangan = ''
+
 
 function balikString(kata) {
-
+    var pengulangan = ''
     for (i = kata.length-1; i >= 0; i--) 
     {
+
     pengulangan = pengulangan + kata[i]
     }  
+    
     return pengulangan  
 }
     console.log(balikString('helloooo world!'))
