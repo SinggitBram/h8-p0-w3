7011b2b336a16514e795f0bae7ffc331d8b7f500
Singgit Bramantha <bambadom@gmail.com>
2019-08-05 13:40:59 +0700

    exercise 2 dan 3

---
+++ exercise2.js
@@ -0,0 +1,12 @@
+
+var pengulangan = ''
+
+function balikString(kata) {
+
+    for (i = kata.length-1; i >= 0; i--) 
+    {
+    pengulangan = pengulangan + kata[i]
+    }  
+    return pengulangan  
+}
+    console.log(balikString('helloooo world!'))
